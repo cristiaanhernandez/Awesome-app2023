fa483f800a45aea958bc23e55eccb3cabcba63bb
Emm152 <124694137+Emm152@users.noreply.github.com>
2023-05-11 14:52:10 +0000

    ⭐ Adding Middleware

--- app.js
+++ app.js
@@ -5,7 +5,29 @@ import express from 'express';
 
 //Crear una instancia de express
 const app = express();
-
+//Registrar nuestro primer middleware
+app.use((req,res,next) =>{
+    console.log("📣 Ejecuntando Middleware 1");
+    //invocando al siguente middleware
+    next();
+});
+app.use((req,res,next) =>{
+    console.log("⭐ Ejecuntando Middleware 2");
+    //invocando al siguente middleware
+    next();
+});
+app.use((req,res,next) =>{
+    console.log(`✨${req.method} - ${req.url}`);
+    //invocando al siguente middleware
+    next();
+});
+app.use((req,res,) =>{
+    console.log("⭐ Respondiendo al cliente");
+    res.send(`
+    <h1>Welcome to Express</h1>
+    <p>This is my awesome app</p>
+    `)
+});
 //Creando el servidor
 const server = http.createServer(app);
 
@@ -17,5 +39,5 @@ const ip    = "0.0.0.0";
 server.listen(port, ip,(err)=>{
     console.log("📣 Sirviendo en http://localhost:3000");
     console.log(`📣 Sirviendo en http://${process.env.IP}:${process.env.PORT}`);
-    
+
 })

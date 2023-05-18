d4f0fb1ee191c1e1d82dd51a19c55bffe051694e
Emm152 <124694137+Emm152@users.noreply.github.com>
2023-05-18 18:43:26 +0000

    ⭐Añadiendo Enrutador

--- app.js
+++ app.js
@@ -1,64 +1,28 @@
-//Importar e l modulo http
-import http from 'http';
 //importando expressjs
 import express from 'express';
+//Importando el enrutador
+import adminRouter from './routes/admin.routes.js';
+import shopRouter from './routes/shop.routes.js';
 
 //Crear una instancia de express
-const app = express({extended: true});
+const app = express();
 
-//Middleware de proceso de datos del cliente
-app.use(express.urlencoded());
-//Registrar nuestro primer middleware
-app.use((req,res,next) =>{
-    console.log("📣 Ejecuntando Middleware 1");
-    //invocando al siguente middleware
-    next();
-});
-app.use('/about', (req, res)=>{
-    res.send(`
-    <h1 style="color: teal">About...</h1>
-    <p style="color: #555"> Esta pagina creada para aprender 
-    desarrollo web en Fullstack con JS</p>
-    `);
-   });
-   //GET /add-product
-app.get('/add-product',(req,res,next)=>{
-    console.log("📣Siriviendo el formulario")
-    //Sirviendo el formularop
-    res.send(`
-    <form action="/add-product" method="POST">
-    <label for="title">Title</label>
-    <input id="title"type="text" name="title">
-    <label for="description">Descripcion</label>
-    <input id="description" type="text" name="description">
-    <button type="submit">Add Product</button>
-    </form>`);
-});
-//POST /add-product
-app.post('/add-product',(req,res)=>{
-    //Realizando extracción de los datos en la peticion
-    for(const prop in req.body){
-        console.log(`${prop}: ${req.body[prop]}`);
-    }
-    //Redirecciónamiento
-    res.redirect('/');
-});
-app.use((req,res,) =>{
-    console.log("⭐ Respondiendo al cliente");
-    res.send(`
-    <h1>Welcome to Express</h1>
-    <p>This is my awesome app</p>
-    `)
-});
-//Creando el servidor
-const server = http.createServer(app);
+//Middleware de proceso de bosy-pasrser
+app.use(express.urlencoded({extended: true}));
+// Se agrega ruta de administrador
+app.use(adminRouter);
+
+// Se agrega ruta de administrador
+app.use(adminRouter);
+// Se agrega ruta shop
+app.use(shopRouter);
 
 //Definir puertos
 const port  = 3000;
 const ip    = "0.0.0.0";
 
 //Arrancando el server
-server.listen(port, ip,(err)=>{
+app.listen(port, ip,(err)=>{
     console.log("📣 Sirviendo en http://localhost:3000");
-    console.log(`📣 Sirviendo en http://${process.env.IP}:${process.env.PORT}`);
+    //console.log(`📣 Sirviendo en http://${process.env.IP}:${process.env.PORT}`);
 })

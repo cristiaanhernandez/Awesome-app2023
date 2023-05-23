8ed8dbeac0d4dde01d919aa329b53ebd242a2df3
Emm152 <124694137+Emm152@users.noreply.github.com>
2023-05-23 13:55:03 +0000

    ⭐Añadiendo 404

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 //importando expressjs
 import express from 'express';
+//Importando http-status
+import httpStatus from 'http-status';
+//Importando Path
+import path from 'path';
 //Importando el enrutador
 import adminRouter from './routes/admin.routes.js';
 import shopRouter from './routes/shop.routes.js';
@@ -7,22 +11,25 @@ import shopRouter from './routes/shop.routes.js';
 //Crear una instancia de express
 const app = express();
 
+
 //Middleware de proceso de bosy-pasrser
-app.use(express.urlencoded({extended: true}));
-// Se agrega ruta de administrador
-app.use(adminRouter);
+app.use(express.urlencoded({ extended: true }));
 
 // Se agrega ruta de administrador
 app.use(adminRouter);
 // Se agrega ruta shop
 app.use(shopRouter);
+//Registrando Middleware Para el error 404
+app.use((req, res) => {
+    res.status(httpStatus.NOT_FOUND).sendFile(path.resolve('views', '404.html'));
+});
 
 //Definir puertos
-const port  = 3000;
-const ip    = "0.0.0.0";
+const port = 3000;
+const ip = "0.0.0.0";
 
 //Arrancando el server
-app.listen(port, ip,(err)=>{
+app.listen(port, ip, (err) => {
     console.log("📣 Sirviendo en http://localhost:3000");
     //console.log(`📣 Sirviendo en http://${process.env.IP}:${process.env.PORT}`);
 })
